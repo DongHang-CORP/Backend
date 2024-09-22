1785e0208e9f578b79b55c97eaf05e2a6da12e93
장영재 <urinaner@naver.com>
2024-09-22 14:15:26 +0900

    #18 feat: 이메일 Service, Controller 추가

--- src/main/java/org/example/food/service/MailService.java
+++ src/main/java/org/example/food/service/MailService.java
@@ -3,7 +3,6 @@ package org.example.food.service;
 import jakarta.mail.internet.MimeMessage;
 
 public interface MailService {
-    static void createNumber() {}
     MimeMessage CreateMail(String mail);
     int sendMail(String mail);
 }
