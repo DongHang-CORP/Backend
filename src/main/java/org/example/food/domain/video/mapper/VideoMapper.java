68fa2e11ab290e9b91d1a14156c86b38e57fa158
장영재 <urinaner@naver.com>
2024-09-29 16:41:39 +0900

    feat: CentralMapper 적용

--- src/main/java/org/example/food/domain/video/mapper/VideoMapper.java
+++ src/main/java/org/example/food/domain/video/mapper/VideoMapper.java
@@ -1,12 +1,13 @@
 package org.example.food.domain.video.mapper;
 
+import org.example.food.common.config.CentralMapperConfig;
 import org.example.food.domain.video.Video;
 import org.example.food.domain.video.dto.VideoReqDto;
 import org.example.food.domain.video.dto.VideoResDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring")
+@Mapper(config = CentralMapperConfig.class, componentModel = "spring")
 public interface VideoMapper {
     Video toEntity(VideoReqDto videoReqDto);
 
