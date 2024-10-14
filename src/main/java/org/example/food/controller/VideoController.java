1c31f8905bf74b15554060a2172bdcf6a82eeeb5
장영재 <urinaner@naver.com>
2024-10-14 14:17:57 +0900

    #48 fix: Api 통일

--- src/main/java/org/example/food/controller/VideoController.java
+++ src/main/java/org/example/food/controller/VideoController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/videos")
+@RequestMapping("/api/videos")
 public class VideoController {
 
     private final VideoService videoService;
