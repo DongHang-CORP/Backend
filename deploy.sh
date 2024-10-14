50b4f1d5250b16fbae120ee6cd7d3da44440e45c
qualificationalitated <hk23g@fsu.edu>
2024-10-14 18:37:55 +0900

    kubeconfig 경로 수정

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
-bash#!/bin/bash
+# NKS 클러스터 접속 설정
+#!/bin/bash
 
 # NKS 클러스터 접속 설정
-export KUBECONFIG=/path/to/kubeconfig # nks 연결 후 설정 예정
+export KUBECONFIG=./kubeconfig.yaml  # kubeconfig 파일 경로
 
 # 애플리케이션 배포
 kubectl apply -f kubernetes/deployment.yaml
