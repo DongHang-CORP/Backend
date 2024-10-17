a6aab400b021879e4e459f3e19cd8895c6df3481
장영재 <urinaner@naver.com>
2024-10-17 15:57:44 +0900

    #58 feat: Like 엔티티 추가

---
+++ src/main/java/org/example/food/domain/like/Like.java
@@ -0,0 +1,33 @@
+package org.example.food.domain.like;
+
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.food.domain.user.User;
+import org.example.food.domain.video.Video;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_post_id")
+    private Video video;
+
+    @Builder
+    public Like(User user, Video video) {
+        this.user = user;
+        this.video = video;
+    }
+}
