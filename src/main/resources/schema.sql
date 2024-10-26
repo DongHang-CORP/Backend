2353eb8f46fb5c60e24fae7b329c7e376955f1fb
장영재 <urinaner@naver.com>
2024-10-26 13:47:22 +0900

    feat: data.sql jpa가 완료된 후 삽입

--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE restaurant (
-                            id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                            name VARCHAR(255),
-                            category VARCHAR(255),
-                            lat DOUBLE,
-                            lng DOUBLE
-);
