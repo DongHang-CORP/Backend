7a4e41b88238bb8cd34d7a3c9385f2afba1549d4
장영재 <urinaner@naver.com>
2024-10-30 03:21:09 +0900

    refactor: Bulder 적용

--- src/main/java/org/example/food/video/dto/VideoResDto.java
+++ src/main/java/org/example/food/video/dto/VideoResDto.java
@@ -18,6 +18,19 @@ public class VideoResDto {
     private int likeCount;
     private boolean isLike;
 
+    @Builder
+    private VideoResDto(Long videoId, String userNickname, Long restaurantId, String restaurant, String url, String content, Category category, int likeCount, boolean isLike) {
+        this.videoId = videoId;
+        this.userNickname = userNickname;
+        this.restaurantId = restaurantId;
+        this.restaurant = restaurant;
+        this.url = url;
+        this.content = content;
+        this.category = category;
+        this.likeCount = likeCount;
+        this.isLike = isLike;
+    }
+
     public static VideoResDto fromVideo(Video video, boolean isLike) {
         return VideoResDto.builder()
                 .videoId(video.getId())
@@ -32,4 +45,16 @@ public class VideoResDto {
                 .build();
     }
 
+    public static VideoResDto of(Video video) {
+        return VideoResDto.builder()
+                .videoId(video.getId())
+                .userNickname(video.getUser().getNickname())
+                .restaurantId(video.getRestaurant().getId())
+                .restaurant(video.getRestaurant().getName())
+                .url(video.getUrl())
+                .content(video.getContent())
+                .category(video.getCategory())
+                .likeCount(video.getLikeCount())
+                .build();
+    }
 }
