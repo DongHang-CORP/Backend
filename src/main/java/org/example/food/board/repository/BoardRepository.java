a9d41d5c2eef693fa5eb9335b941759986992162
장영재 <urinaner@naver.com>
2025-01-31 18:43:34 +0900

    refactor: 엔티티그래프 적용

--- src/main/java/org/example/food/board/repository/BoardRepository.java
+++ src/main/java/org/example/food/board/repository/BoardRepository.java
@@ -4,11 +4,13 @@ import org.example.food.board.entity.Category;
 import org.example.food.board.entity.Board;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
@@ -24,5 +26,12 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     @Query(value = "UPDATE board SET like_count = like_count - 1 WHERE board_id = :boardId", nativeQuery = true)
     void decreaseLikeCount(Long boardId);
 
-    Slice<Board> findSliceBy(final Pageable pageable);
+    @Query("SELECT b FROM Board b")
+    @EntityGraph(attributePaths = {"comments", "user"})
+    Slice<Board> findSliceBy(Pageable pageable);
+
+    @Query("SELECT b FROM Board b WHERE b.id = :boardId")
+    @EntityGraph(attributePaths = {"comments", "user"})
+    Board findBoardWithComments(@Param("boardId") Long boardId);
+
 }
